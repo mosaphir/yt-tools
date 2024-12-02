5df94613e13dc216c10009ba04abcae98aaeacdd
Mosaphir <skdm0111@gmail.com>
2024-12-02 23:47:27 +0600

    Create whois.js

---
+++ pages/api/whois.js
@@ -0,0 +1,16 @@
+import whois from 'whois-json';
+
+export default async function handler(req, res) {
+  const { domain } = req.query;
+
+  if (!domain) {
+    return res.status(400).json({ error: 'Domain is required.' });
+  }
+
+  try {
+    const data = await whois(domain);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Unable to retrieve hosting information.' });
+  }
+}
