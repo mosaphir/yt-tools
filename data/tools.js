86b44bf2754122b8be1efb8b24fc8fad7e3635f5
Mosaphir <skdm0111@gmail.com>
2024-12-02 13:29:22 +0600

    Create tools.js

---
+++ data/tools.js
@@ -0,0 +1,16 @@
+const tools = [
+  { name: 'MD5 Generator', link: '/tools/md5-generator', description: 'Generate MD5 hashes from text.' },
+  { name: 'What Is My IP', link: '/tools/what-is-my-ip', description: 'Find out your public IP address.' },
+  { name: 'IP Address Lookup', link: '/tools/ip-lookup', description: 'Look up detailed information about an IP address.' },
+  { name: 'Base64 Decode', link: '/tools/base64-decode', description: 'Decode Base64 encoded strings.' },
+  { name: 'Base64 Encode', link: '/tools/base64-encode', description: 'Encode text into Base64 format.' },
+  { name: 'Color Converter', link: '/tools/color-converter', description: 'Convert between HEX, RGB, and other color formats.' },
+  { name: 'Password Generator', link: '/tools/password-generator', description: 'Generate secure random passwords.' },
+  { name: 'VTT to SRT', link: '/tools/vtt-to-srt', description: 'Convert VTT subtitle files to SRT format.' },
+  { name: 'SRT to VTT', link: '/tools/srt-to-vtt', description: 'Convert SRT subtitle files to VTT format.' },
+  { name: 'YouTube Thumbnail Downloader', link: '/tools/youtube-thumbnail-downloader', description: 'Download thumbnails from YouTube videos.' },
+  { name: 'HEX to RGB', link: '/tools/hex-to-rgb', description: 'Convert HEX color codes to RGB format.' },
+  { name: 'RGB to HEX', link: '/tools/rgb-to-hex', description: 'Convert RGB color codes to HEX format.' },
+];
+
+export default tools;
